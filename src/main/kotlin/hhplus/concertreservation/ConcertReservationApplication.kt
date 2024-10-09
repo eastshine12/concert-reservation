e239c3203e36f2cade41c8bd29cef67705e458c1
EastShine <eastshine_@naver.com>
2024-10-09 11:57:15 +0900

    Initial commit

---
+++ src/main/kotlin/hhplus/concertreservation/ConcertReservationApplication.kt
@@ -0,0 +1,11 @@
+package hhplus.concertreservation
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ConcertReservationApplication
+
+fun main(args: Array<String>) {
+    runApplication<ConcertReservationApplication>(*args)
+}
---
+++ src/test/kotlin/hhplus/concertreservation/ConcertReservationApplicationTests.kt
@@ -0,0 +1,13 @@
+package hhplus.concertreservation
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ConcertReservationApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
