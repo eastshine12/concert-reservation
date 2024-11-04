b4bfe52b203b13e41d4b8979e2dc3229bab4b560
eastshine <eastshine@pointi.com>
2024-11-04 15:35:12 +0900

    Fix: Spring-Retry 제거

--- src/main/kotlin/hhplus/concertreservation/ConcertReservationApplication.kt
+++ src/main/kotlin/hhplus/concertreservation/ConcertReservationApplication.kt
@@ -4,10 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
-import org.springframework.retry.annotation.EnableRetry
 import org.springframework.scheduling.annotation.EnableScheduling
 
-@EnableRetry
 @EnableJpaAuditing
 @EnableScheduling
 @ConfigurationPropertiesScan
--- src/main/kotlin/hhplus/concertreservation/domain/user/service/UserService.kt
+++ src/main/kotlin/hhplus/concertreservation/domain/user/service/UserService.kt
@@ -9,9 +9,6 @@ import hhplus.concertreservation.domain.user.entity.User
 import hhplus.concertreservation.domain.user.repository.BalanceHistoryRepository
 import hhplus.concertreservation.domain.user.repository.UserRepository
 import hhplus.concertreservation.domain.user.toUpdateBalanceInfo
-import org.springframework.orm.ObjectOptimisticLockingFailureException
-import org.springframework.retry.annotation.Backoff
-import org.springframework.retry.annotation.Retryable
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import java.math.BigDecimal
@@ -37,11 +34,6 @@ class UserService(
     }
 
     @Transactional
-    @Retryable(
-        value = [ObjectOptimisticLockingFailureException::class],
-        maxAttempts = 3,
-        backoff = Backoff(delay = 100),
-    )
     fun updateUserBalance(
         userId: Long,
         amount: BigDecimal,
