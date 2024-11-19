6c7d210478e7d6cc316dafe5a73a503a71aa1310
EastShine <eastshine_@naver.com>
2024-11-20 02:21:18 +0900

    Feat: 카프카 이벤트 발행 후 consume하여 outbox 상태를 변경하는 kafkaListener 작성

---
+++ src/main/kotlin/hhplus/concertreservation/interfaces/consumer/OutboxEventConsumer.kt
@@ -0,0 +1,27 @@
+package hhplus.concertreservation.interfaces.consumer
+
+import hhplus.concertreservation.domain.common.enums.OutboxStatus
+import hhplus.concertreservation.domain.outbox.OutboxRepository
+import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.kafka.support.KafkaHeaders
+import org.springframework.messaging.handler.annotation.Header
+import org.springframework.stereotype.Component
+
+@Component
+class OutboxEventConsumer(
+    private val outboxRepository: OutboxRepository,
+) {
+    @KafkaListener(topics = ["concert.reservation.created"], groupId = "outbox-consumer-group")
+    fun consumeMessage(key: String, payload: String, @Header(KafkaHeaders.RECEIVED_TOPIC) topic: String) {
+        val eventType = when (topic) {
+            "concert.reservation.created" -> "RESERVATION_CREATED"
+            else -> return
+        }
+
+        val outboxEvent = outboxRepository.findByEventTypeAndKey(eventType, key)
+        if (outboxEvent != null && outboxEvent.status == OutboxStatus.FAILED) {
+            outboxEvent.confirmPublish()
+            outboxRepository.save(outboxEvent)
+        }
+    }
+}
